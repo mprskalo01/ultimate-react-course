c89ff244b185091b621348c4cec2af2048483766
Mario <mario.prskalo5@gmail.com>
2025-01-18 20:46:16 +0100

    components in react

--- 03-pizza-menu/starter/src/App.tsx
+++ 03-pizza-menu/starter/src/App.tsx
@@ -1,35 +1,11 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
+import { Pizza } from './components/Pizza';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const incrementCount = (): void => setCount((count) => count + 1);
-  const lang = navigator.language;
-  console.log(lang);
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Hello Vite + React</h1>
-      <div className="card">
-        <button onClick={incrementCount}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Hello pizzas!</h1>
+      <Pizza />
     </>
   );
 }
