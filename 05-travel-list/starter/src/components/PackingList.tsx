b6e433b6fa70769f6c791fc953b5a873fa230ae4
Mario <mario.prskalo5@gmail.com>
2025-01-23 21:40:35 +0100

    adding sorting functionality

--- 05-travel-list/starter/src/components/PackingList.tsx
+++ 05-travel-list/starter/src/components/PackingList.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Item, { ItemInterface } from './Item';
 
 interface Props {
@@ -7,10 +8,23 @@ interface Props {
 }
 
 export function PackingList({ items, onDeleteItem, onToggleItem }: Props) {
+  const [sortBy, setSortBy] = useState<string>('input');
+
+  let sortedItems = items;
+  if (sortBy === 'input') sortedItems = items;
+  if (sortBy === 'description')
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+
+  if (sortBy === 'packed')
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
   return (
     <div className="list">
       <ul>
-        {items.map((item: ItemInterface) => (
+        {sortedItems.map((item: ItemInterface) => (
           <Item
             item={item}
             key={item.id}
@@ -19,6 +33,16 @@ export function PackingList({ items, onDeleteItem, onToggleItem }: Props) {
           />
         ))}
       </ul>
+      <div className="actions">
+        <select
+          value={sortBy}
+          onChange={(event) => setSortBy(event.target.value)}
+        >
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description </option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
