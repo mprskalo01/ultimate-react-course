f86c9e402daac1952bb24dcc7a89d486e1354e93
Mario <mario.prskalo5@gmail.com>
2025-01-23 16:42:47 +0100

    date counter v2 challenge complete

--- challenges/src/App.tsx
+++ challenges/src/App.tsx
@@ -1,14 +1,14 @@
 import './App.css';
-import FlashCardList from './components/FlashCards/FlashCardList';
-// import DateCounter from './components/DateCounter/DateCounter';
+// import FlashCardList from './components/FlashCards/FlashCardList';
+import DateCounter from './components/DateCounter/DateCounter';
 // import ProfileCard from './components/ProfileCard/ProfileCard';
 
 function App() {
   return (
     <>
       {/* <ProfileCard /> */}
-      {/* <DateCounter /> */}
-      <FlashCardList />
+      <DateCounter />
+      {/* <FlashCardList /> */}
     </>
   );
 }
--- challenges/src/components/DateCounter/DateCounter.tsx
+++ challenges/src/components/DateCounter/DateCounter.tsx
@@ -12,11 +12,16 @@ function DateCounter() {
     setCount((count) => count - step);
   };
   const increaseStep = () => {
-    setStep((step) => step + 1);
+    if (step < 10) setStep((step) => step + 1);
   };
   const decreaseStep = () => {
     if (step > 1) setStep((step) => step - 1);
   };
+
+  function handleReset() {
+    setCount(0);
+    setStep(1);
+  }
 
   const todayDate = new Date('january 23 2025');
   todayDate.setDate(todayDate.getDate() + count);
@@ -24,23 +29,38 @@ function DateCounter() {
   return (
     <div className="date-counter">
       <div>
+        <button onClick={decreaseStep}>-</button>
+        <input
+          type="range"
+          min="1"
+          max="10"
+          value={step}
+          onChange={(event) => setStep(Number(event.target.value))}
+        />
+        <span>Step: {step}</span>
+        <button onClick={increaseStep}>+</button>
+      </div>
+      <div>
         <button onClick={decreaseCount}>-</button>
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
         <span>Count: {count}</span>
         <button onClick={increaseCount}>+</button>
       </div>
       <div>
-        <button onClick={decreaseStep}>-</button>
-        <span>Step: {step}</span>
-        <button onClick={increaseStep}>+</button>
+        <span>
+          {count === 0
+            ? 'Today is '
+            : count > 0
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{todayDate.toDateString()}</span>
       </div>
-      <span>
-        {count === 0
-          ? 'Today is '
-          : count > 0
-          ? `${count} days from today is `
-          : `${Math.abs(count)} days ago was `}
-      </span>
-      <span>{todayDate.toDateString()}</span>
+      <button onClick={handleReset}>Reset</button>
     </div>
   );
 }
