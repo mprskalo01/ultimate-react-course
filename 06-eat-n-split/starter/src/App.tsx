b915b60fc10f3f1ffbead2c9d44d5cf5c719ae3d
Mario <mario.prskalo5@gmail.com>
2025-01-24 00:11:31 +0100

    initializing files for eat n split practice project

---
+++ 06-eat-n-split/starter/src/App.tsx
@@ -0,0 +1,10 @@
+import FriendCard from './components/FriendCard';
+function App() {
+  return (
+    <>
+      <FriendCard />
+    </>
+  );
+}
+
+export default App;
---
+++ 06-eat-n-split/starter/src/components/FriendCard.tsx
@@ -0,0 +1,37 @@
+export default function FriendCard() {
+  interface Friend {
+    id: number;
+    name: string;
+    image: string;
+    balance: number;
+  }
+
+  const initialFriends: Friend[] = [
+    {
+      id: 118836,
+      name: 'Clark',
+      image: 'https://i.pravatar.cc/48?u=118836',
+      balance: -7,
+    },
+    {
+      id: 933372,
+      name: 'Sarah',
+      image: 'https://i.pravatar.cc/48?u=933372',
+      balance: 20,
+    },
+    {
+      id: 499476,
+      name: 'Anthony',
+      image: 'https://i.pravatar.cc/48?u=499476',
+      balance: 0,
+    },
+  ];
+
+  return (
+    <div>
+      {initialFriends.map((friend) => (
+        <h1>{friend.name}</h1>
+      ))}
+    </div>
+  );
+}
