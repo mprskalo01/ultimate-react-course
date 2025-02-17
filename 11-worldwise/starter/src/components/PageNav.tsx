eff24cb8c3542c2dec6508f5cc142365b882a1a2
Mario <mario.prskalo5@gmail.com>
2025-02-17 13:10:43 +0100

    building the pages

--- 11-worldwise/starter/src/components/PageNav.tsx
+++ 11-worldwise/starter/src/components/PageNav.tsx
@@ -1,18 +1,22 @@
+import Logo from './Logo';
 import styles from './PageNav.module.css';
 import { NavLink } from 'react-router';
 
 const PageNav = () => {
   return (
     <nav className={styles.nav}>
+      <Logo />
       <ul>
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
         <li>
           <NavLink to="/pricing">Pricing</NavLink>
         </li>
         <li>
           <NavLink to="/product">Product</NavLink>
+        </li>
+        <li>
+          <NavLink to="/login" className={styles.ctaLink}>
+            Login
+          </NavLink>
         </li>
       </ul>
     </nav>
--- 11-worldwise/starter/src/pages/Homepage.tsx
+++ 11-worldwise/starter/src/pages/Homepage.tsx
@@ -1,12 +1,11 @@
 import styles from './Homepage.module.css';
 import PageNav from '../components/PageNav';
-import AppNav from '../components/AppNav';
+import { Link } from 'react-router';
 
 export default function Homepage() {
   return (
     <main className={styles.homepage}>
       <PageNav />
-      <AppNav />
       <section>
         <h1>
           You travel the world.
@@ -18,6 +17,9 @@ export default function Homepage() {
           of. Never forget your wonderful experiences, and show your friends how
           you have wandered the world.
         </h2>
+        <Link to="app" className="cta">
+          Start tracking now
+        </Link>
       </section>
     </main>
   );
